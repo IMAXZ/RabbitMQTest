4b50726ac174855094bc99fd1c5efbd53d8a65a0
zx <111@111.com>
2019-11-05 22:25:01 +0800

    官方第一个小例子

---
+++ DotNetRabbitMQ/P2PReceive/Receive.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace P2PReceive
+{
+    class Receive
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() 
+            {
+                HostName = "49.232.164.202",
+                UserName = "admin",
+                Password = "admin",
+                Port = AmqpTcpEndpoint.UseDefaultPort,
+                VirtualHost = "/",
+                Protocol = Protocols.DefaultProtocol
+            };
+            using (var connection = factory.CreateConnection())
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                                   durable: false,
+                                                   exclusive: false,
+                                                   autoDelete: false,
+                                                   arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("[x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "hello",
+                                                    autoAck: true,
+                                                    consumer: consumer);
+                Console.WriteLine("Press [Enter] to exit.");
+                Console.ReadLine();
+            }   
+        }
+    }
+}
---
+++ DotNetRabbitMQ/P2PSend/Send.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace P2PSend
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "49.232.164.202",
+                UserName = "admin",
+                Password = "admin",
+                Port = AmqpTcpEndpoint.UseDefaultPort,
+                VirtualHost = "/",
+                Protocol = Protocols.DefaultProtocol
+            };
+            using (var connection = factory.CreateConnection())
+            
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello", 
+                                                       durable: false, 
+                                                       exclusive: false,
+                                                       autoDelete: false, 
+                                                       arguments: null);
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "",
+                                                    routingKey: "hello",
+                                                    basicProperties: null,
+                                                    body: body);
+                    Console.WriteLine("[x] sent {0}", message);
+                }
+            Console.WriteLine("Press [Enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
