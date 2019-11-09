b9742451f721e3ea19b51a3cc5ef190014d325cb
郑鑫 <822262059@qq.com>
2019-11-09 23:11:55 +0800

    订阅模式fanout模式

---
+++ DotNetRabbitMQ/ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using Utils;
+
+namespace ReceiveLogs
+{
+    class ReceiveLogs
+    {
+        static void Main(string[] args)
+        {
+            var factory = ConnectionFactoryUtils.GetConnectionFactory();
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+                var queneName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queneName,
+                    exchange: "logs",
+                    routingKey: ""
+                    );
+                Console.WriteLine(" [*] Waiting for logs.");
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] {0}", message);
+                };
+                channel.BasicConsume(queue: queneName, autoAck: true, consumer: consumer);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
