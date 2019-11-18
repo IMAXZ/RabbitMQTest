a43e7ceffd20db6032766769bdb1ffe228da715c
郑鑫 <822262059@qq.com>
2019-11-18 21:34:55 +0800

    路由模式

---
+++ DotNetRabbitMQ/EmitLogDirect/EmitLogDirect.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utils;
+
+namespace EmitLogDirect
+{
+    class EmitLogDirect
+    {
+        static void Main(string[] args)
+        {
+            var factory = ConnectionFactoryUtils.GetConnectionFactory();
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs",
+                                    type: "direct");
+                var severity = args.Length > 0 ? args[0] : "info";
+                var message = args.Length > 1 ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "direct_logs",
+                                                routingKey: severity,
+                                                basicProperties: null,
+                                                body: body
+                                                );
+                Console.WriteLine("[x] Sent '{0}':'{1}'", severity, message);
+            }
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
